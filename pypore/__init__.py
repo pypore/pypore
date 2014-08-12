cfb5ecd578a11ff083f35e61269f3b91c554fd5f
Will Parkin <wmparkin@gmail.com>
2014-08-12 15:14:50 -0400

    added tests/implemented pypore.open for opening files based on extension

--- pypore/__init__.py
+++ pypore/__init__.py
@@ -1,1 +1,29 @@
-__author__ = 'will'
+def open(filename, reader_class=None):
+    """
+    Opens a read only, raw current data file of one of the following formats:
+
+        #. Chimera files. See implementations in :py:mod:`pypore.i_o.chimera_reader`
+        #. Heka files. See implementations in :py:mod:`pypore.i_o.heka_reader`
+
+    To implement your own reader, extend :py:class:`pypore.i_o.abstract_reader.AbstractReader`.
+    :param filename: Filename to open.
+    :param reader_class: (Optional) A reader class to be used to read the filename. If None, a reader class will be
+    chosen based on the file extension.
+    :return: An open reader.
+    """
+
+    if reader_class is not None:
+        return reader_class(filename)
+
+    if filename[-len('.log'):] == '.log':
+        from pypore.i_o.chimera_reader import ChimeraReader
+        reader_class = ChimeraReader
+    elif filename[-len('.hkd'):] == '.hkd':
+        from pypore.i_o.heka_reader import HekaReader
+        reader_class = HekaReader
+
+    if reader_class is None:
+        import os
+        raise ValueError("No reader was found for the file extension '{0}'.".format(os.path.splitext(filename)[1]))
+
+    return reader_class(filename)
