2b7bc82f9c61e097d3320b88de33bcfe871672a2
Will Parkin <wmparkin@gmail.com>
2014-08-11 21:18:24 -0400

    added HekaReader tests

---
+++ pypore/i_o/tests/test_heka_reader.py
@@ -0,0 +1,29 @@
+import unittest
+
+from pypore.i_o.heka_reader import HekaReader
+from pypore.i_o.tests.reader_tests import ReaderTests
+import pypore.sampledata.testing_files as tf
+
+
+class TestHekaReader(unittest.TestCase, ReaderTests):
+    reader_class = HekaReader
+
+    def help_scaling(self):
+        filename = tf.get_abs_path('heka_1.5s_mean5.32p_std2.76p.hkd')
+        mean = 5.32e-12
+        std_dev = 2.76e-12
+        return [filename], [mean], [std_dev]
+
+    def help_slicing(self):
+        filename = tf.get_abs_path('heka_1.5s_mean5.32p_std2.76p.hkd')
+        return [filename]
+
+    def help_sample_rate(self):
+        filename = tf.get_abs_path('heka_1.5s_mean5.32p_std2.76p.hkd')
+        sample_rate = 50000.0
+        return [filename], [sample_rate]
+
+    def help_shape(self):
+        filename = tf.get_abs_path('heka_1.5s_mean5.32p_std2.76p.hkd')
+        shape = (75000,)
+        return [filename], [shape]
